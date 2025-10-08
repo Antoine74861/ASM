77e28fbcc804917d5c3eb3462c72b6a53143c85c
Antoine <209369232+Antoine74861@users.noreply.github.com>
2025-10-08 15:30:51 +0200

    Premier exo version "moche" (j'utilise dec sans verifier si c'est bien un \n)

--- Exos/hello_name.asm
+++ Exos/hello_name.asm
@@ -8,11 +8,16 @@
 
 ;  build: nasm -f elf64 -o hello_name.o hello_name.asm
 ;  link:  ld -o hello_name hello_name.o
-;  les deux: nasm -f elf64 -o hello_name.o hello_name.asm && ld -o hello_name hello_name.o
+;  les deux: nasm -f elf64 -o hello_name.o hello_name.asm && ld -o hello_name hello_name.o && ./hello_name
 
 SECTION .rodata
 AskNameInput:        db "Entrez votre prénom: "
 len_AskNameInput:    equ $ - AskNameInput
+AnswerName:        db "Bonjour "
+len_AnswerName:    equ $ - AnswerName
+AnswerExclamationMark:        db "!", 10
+len_AnswerExclamationMark:    equ $ - AnswerExclamationMark
+user_input_buf       equ 256
 
 SECTION .bss
 user_input: resb 256
@@ -31,9 +36,33 @@ _start:
     mov     rax, 0 				  ; ID du syscall
     mov     rdi, 0				  ; unsigned int fd
     mov     rsi, user_input		  ; char *buf
-    mov     rdx, 256        	  ; size_t count
+    mov     rdx, user_input_buf   ; size_t count
     syscall
+
+	mov r12, rax                  ; on met rax (le retour de read) dans rcx
+    dec r12                       ; enleve le dernier char (tres moche)
 	
+    ; write
+    mov     rax, 1 				  ; ID du syscall
+    mov     rdi, 1				  ; unsigned int fd
+    mov     rsi, AnswerName  	  ; const char *buf
+    mov     rdx, len_AnswerName   ; size_t count
+    syscall	
+
+	; write
+    mov     rax, 1 				  ; ID du syscall
+    mov     rdi, 1				  ; unsigned int fd
+    mov     rsi, user_input	  	  ; const char *buf
+    mov     rdx, r12              ; size_t count
+    syscall	
+
+    ; write
+    mov     rax, 1 				            ; ID du syscall
+    mov     rdi, 1				            ; unsigned int fd
+    mov     rsi, AnswerExclamationMark	    ; const char *buf
+    mov     rdx, len_AnswerExclamationMark  ; size_t count
+    syscall	
+
     ; exit(0)
     mov     rax, 60
     xor     rdi, rdi
